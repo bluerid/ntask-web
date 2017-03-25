383f911f6affd834434ae15c5662640c20bdafe0
Keymaker <palash@itobuz.com>
2017-03-25 16:12:33 +0530

    Client App for ntask api

---
+++ src/ntask.js
@@ -0,0 +1,12 @@
+import TimeEmitter from "tiny-emitter";
+import request from "browser-request";
+
+class NTask extends TinyEmitter {
+	constructor() {
+		super();
+		this.request = request;
+		this.URL = "http://localhost:3000"
+	}
+}
+
+module.exports = NTask;
